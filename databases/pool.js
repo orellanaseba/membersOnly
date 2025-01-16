309a8fe45ec53f23fc4a5f8734ea5d1f19f6fd5a
Sebastián Orellana <69601416+orellanaseba@users.noreply.github.com>
2025-01-16 00:24:53 -0300

    Update pool.js

--- databases/pool.js
+++ databases/pool.js
@@ -4,11 +4,7 @@ require("dotenv").config();
 const { Pool } = pg
 
 const pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     }

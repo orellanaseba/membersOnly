32473f1cbce0a8dece0c3f832ead99475287460f
Sebastián <orellanasebastian2002@gmail.com>
2025-01-15 22:40:14 -0300

    config

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("node:path");
 const session = require("express-session")
 const passport = require("./config/passportConfig.js")
+const PgSession = require("connect-pg-simple")(session);
+const pool = require("./databases/pool.js");
 
 // Routes
 const indexRouter = require("./routes/indexRouter")
@@ -13,7 +15,21 @@ const registerRouter = require("./routes/registerRouter")
 
 
  // Activa la configuración de Passport
-app.use(session({ secret: "members_only", resave: false, saveUninitialized: false })) 
+app.use(session(
+    {
+        secret: "members_only",
+        resave: false,
+        saveUninitialized: false,
+        store: new PgSession({
+            pool: pool,
+        }),
+        cookie: {
+            secure: false,
+            httpOnly: true
+        }
+    }
+    )
+) 
 app.use(passport.session())
 
 app.use(express.urlencoded({ extended: false }))
@@ -37,5 +53,4 @@ app.use("/feed", feedRouter);
 app.use("/logout", logoutRouter);
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.PGHOST || "localhost"
-app.listen(PORT, () => console.log(`http://${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));

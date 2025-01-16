f3949204e4d052ef277a20811f25c14bf2738c38
Sebastián <orellanasebastian2002@gmail.com>
2025-01-15 23:14:07 -0300

    :)

--- databases/pool.js
+++ databases/pool.js
@@ -3,7 +3,11 @@ const { Pool } = require("pg")
 require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
     ssl: {
         rejectUnauthorized: false,
     }
